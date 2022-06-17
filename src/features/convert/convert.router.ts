107e033d30627909e667ee36aeff6f9bbf4bd007
Quentin Blondel <qb@volpy.com>
2022-06-17 18:23:24 +0200

    clean code

--- src/features/convert/convert.router.ts
+++ src/features/convert/convert.router.ts
@@ -5,7 +5,6 @@
 import express, { Request, Response } from "express";
 import ConvertService from "./convert.service";
 import { Result } from "./convert.interface";
-import SSEService from "../../services/sse";
 
 
 /**
@@ -31,38 +30,3 @@ convertRouter.get("/toroman/:numberStr", (req: Request, res: Response) => {
 		res.status(500).send(e.message);
 	}
 });
-
-
-convertRouter.get("/sse/", async (req: Request, res: Response) => {
-	try {
-		SSEService.init(req, res);
-	} catch (e: any) {
-		res.status(500).send(e.message);
-	}
-});
-
-convertRouter.get("/sse/toroman/:numberStr", async (req: Request, res: Response) => {
-	try {
-		const result: Result = ConvertService.toRoman(req.params.numberStr);
-		console.log("SSE result", result);
-		SSEService.push(result);
-	} catch (e: any) {
-		res.status(500).send(e.message);
-	}
-});
-
-// convertRouter.get("/sse/toroman/:numberStr", async (req: Request, res: Response) => {
-// 	try {
-// 		const session = await createSession(req, res);
-// 		const result: Result = ConvertService.toRoman(req.params.numberStr);
-
-// 		if (result.errorCode === 0) {
-// 			return session.push(result);
-// 		}
-
-// 		res.status(404).send(result);
-
-// 	} catch (e: any) {
-// 		res.status(500).send(e.message);
-// 	}
-// });
