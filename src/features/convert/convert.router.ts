d83bdae4716cd6bb7fe59e1cf96e224b3de7d442
Quentin Blondel <qb@volpy.com>
2022-06-17 18:20:17 +0200

    del not sse feature

--- src/features/convert/convert.router.ts
+++ src/features/convert/convert.router.ts
@@ -13,25 +13,6 @@ import SSEService from "../../services/sse";
  */
 export const convertRouter = express.Router();
 
-/**
- * Controller Definitions
- */
-
-// GET items/:id
-convertRouter.get("/toroman/:numberStr", (req: Request, res: Response) => {
-	try {
-		const result: Result = ConvertService.toRoman(req.params.numberStr);
-
-		if (result.errorCode === 0) {
-			return res.status(200).send(result);
-		}
-
-		res.status(404).send(result);
-	} catch (e: any) {
-		res.status(500).send(e.message);
-	}
-});
-
 
 convertRouter.get("/sse/", async (req: Request, res: Response) => {
 	try {
@@ -51,18 +32,3 @@ convertRouter.get("/sse/toroman/:numberStr", async (req: Request, res: Response)
 	}
 });
 
-// convertRouter.get("/sse/toroman/:numberStr", async (req: Request, res: Response) => {
-// 	try {
-// 		const session = await createSession(req, res);
-// 		const result: Result = ConvertService.toRoman(req.params.numberStr);
-
-// 		if (result.errorCode === 0) {
-// 			return session.push(result);
-// 		}
-
-// 		res.status(404).send(result);
-
-// 	} catch (e: any) {
-// 		res.status(500).send(e.message);
-// 	}
-// });
